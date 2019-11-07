1010113b48a966d69e086b7fb0690695dba43629
Dav1dch <807557852@qq.com>
2019-11-07 20:28:36 +0800

    last one button

--- app/src/main/java/com/tuffy/dial/MainActivity.java
+++ app/src/main/java/com/tuffy/dial/MainActivity.java
@@ -13,12 +13,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.provider.Settings;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,17 +36,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int PERMISS_CONTACT = 1;
     private RecyclerView recyclerView;
     private TextView phonenum;
-    private void init(){
+    private TabHost tabhost;
+
+    /*** init function
+     *
+     */
+    private void init() {
+        addPermissions();
+        initTabHost();
+        initRecyclerView();
+        initBtn();
+    }
+
+    private void initRecyclerView() {
         ArrayList<MyContacts> contacts = getAllContacts(MainActivity.this);
-        findViewById(R.id.dial).setOnClickListener(this);
-        String[] permissList = {Manifest.permission.READ_CONTACTS, Manifest.permission.READ_PHONE_STATE};
-        addPermissByPermissionList(MainActivity.this, permissList, PERMISS_CONTACT);
         phonenum = findViewById(R.id.phoneNum);
         ContactAdapter adapter = new ContactAdapter(contacts, phonenum);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private void initBtn() {
+        findViewById(R.id.dial).setOnClickListener(this);
         findViewById(R.id.btn_one).setOnClickListener(this);
         findViewById(R.id.btn_two).setOnClickListener(this);
         findViewById(R.id.btn_three).setOnClickListener(this);
@@ -54,28 +70,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_eight).setOnClickListener(this);
         findViewById(R.id.btn_nine).setOnClickListener(this);
         findViewById(R.id.btn_back).setOnClickListener(this);
+        findViewById(R.id.btn_sendmessage).setOnClickListener(this);
+    }
+
+
+    private void initTabHost() {
+        tabhost = findViewById(R.id.tabhost);
+        tabhost.setup();
+        LayoutInflater.from(this).inflate(R.layout.dial, tabhost.getTabContentView());
+        LayoutInflater.from(this).inflate(R.layout.message, tabhost.getTabContentView());
+        tabhost.addTab(tabhost.newTabSpec("tab01").setIndicator("打电话", null).setContent(R.id.dial));
+        tabhost.addTab(tabhost.newTabSpec("message").setIndicator("发短信", null).setContent(R.id.message));
+    }
+
+    private void getPermissions() {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float initx = 0;
+        float currentx = 0;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                initx = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                currentx = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                /*左右滑动事件处理*/
+                if ((currentx - initx) > 25) {
+                    if (tabhost.getCurrentTab() != 0) {
+                        tabhost.setCurrentTab(tabhost.getCurrentTab() - 1);
+                    }
+                } else if ((currentx - initx) < -25) {
+                    if (tabhost.getCurrentTab() != tabhost.getTabContentView().getChildCount()) {
+                        tabhost.setCurrentTab(tabhost.getCurrentTab() + 1);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 
     /**
      * 动态权限
      */
-    public void addPermissByPermissionList(Activity activity, String[] permissions, int request) {
-        //Android 6.0开始的动态权限，这里进行版本判断
-            ArrayList<String> mPermissionList = new ArrayList<>();
-            for (int i = 0; i < permissions.length; i++) {
-                if (ContextCompat.checkSelfPermission(activity, permissions[i])
-                        != PackageManager.PERMISSION_GRANTED) {
-                    mPermissionList.add(permissions[i]);
-                }
+    public void addPermissions() {
+        String[] permissionsList = {Manifest.permission.READ_CONTACTS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.SEND_SMS};
+        ArrayList<String> mPermissionList = new ArrayList<>();
+        for (int i = 0; i < permissionsList.length; i++) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, permissionsList[i])
+                    != PackageManager.PERMISSION_GRANTED) {
+                mPermissionList.add(permissionsList[i]);
             }
-            //非初次进入App且已授权
-            if (!mPermissionList.isEmpty()) {
-                //请求权限方法
-                //将List转为数组
-                String[] permissionsNew = mPermissionList.toArray(new String[mPermissionList.size()]);
-                //这个触发下面onRequestPermissionsResult这个回调
-                ActivityCompat.requestPermissions(activity, permissionsNew, request);
-            }
+        }
+        //非初次进入App且已授权
+        if (!mPermissionList.isEmpty()) {
+            //请求权限方法
+            //将List转为数组
+            String[] permissionsNew = mPermissionList.toArray(new String[mPermissionList.size()]);
+            //这个触发下面onRequestPermissionsResult这个回调
+            ActivityCompat.requestPermissions(MainActivity.this, permissionsNew, PERMISS_CONTACT);
+        }
     }
 
     @Override
@@ -87,49 +148,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-       switch (v.getId()){
-           case R.id.dial:
-               if (ContextCompat.checkSelfPermission(MainActivity.this,
-                       Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-                   // 没有获得授权，申请授权
-                   if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                           Manifest.permission.CALL_PHONE)) {
-
-                       // 帮跳转到该应用的设置界面，让用户手动授权
-                       Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                       Uri uri = Uri.fromParts("package", getPackageName(), null);
-                       intent.setData(uri);
-                       startActivity(intent);
-                   }else{
-                       // 不需要解释为何需要该权限，直接请求授权
-                       ActivityCompat.requestPermissions(MainActivity.this,
-                               new String[]{Manifest.permission.CALL_PHONE},
-                               MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                   }
-               }else {
-                   // 已经获得授权，可以打电话
-                   Intent dialIntent = new Intent("android.intent.action.CALL", Uri.parse("tel:" + phonenum.getText()));
-                   startActivity(dialIntent);
-               }
-               break;
-           case R.id.btn_one:
-           case R.id.btn_two:
-           case R.id.btn_three:
-           case R.id.btn_four:
-           case R.id.btn_five:
-           case R.id.btn_six:
-           case R.id.btn_seven:
-           case R.id.btn_eight:
-           case R.id.btn_nine:
-               phonenum.setText(phonenum.getText() + ((Button)v).getText().toString());
-               break;
-           case R.id.btn_back:
-               phonenum.setText(phonenum.getText().subSequence(0, phonenum.getText().length()-1));
-               break;
-               default:
-                   break;
-       }
+        switch (v.getId()) {
+            case R.id.btn_dial:
+                // 已经获得授权，可以打电话
+                Intent dialIntent = new Intent("android.intent.action.CALL", Uri.parse("tel:" + phonenum.getText()));
+                startActivity(dialIntent);
+                break;
+            case R.id.btn_one:
+            case R.id.btn_two:
+            case R.id.btn_three:
+            case R.id.btn_four:
+            case R.id.btn_five:
+            case R.id.btn_six:
+            case R.id.btn_seven:
+            case R.id.btn_eight:
+            case R.id.btn_nine:
+                phonenum.setText(phonenum.getText() + ((Button) v).getText().toString());
+                break;
+            case R.id.btn_back:
+                if (!"".equals(phonenum.getText())){
+                    phonenum.setText(phonenum.getText().subSequence(0, phonenum.getText().length() - 1));
+                }
+                break;
+            case R.id.btn_sendmessage:
+                EditText messagePhone = findViewById(R.id.message_phone);
+                EditText messageContent = findViewById(R.id.message_content);
+                String number = messagePhone.getText().toString();
+                String content = messagePhone.getText().toString();
+                if(!"".equals(number) && !"".equals(content)){
+                    ArrayList<String> message = SmsManager.getDefault().divideMessage(content);
+                    for (String text : message){
+                        SmsManager.getDefault().sendTextMessage(number, null, text, null, null);
+                    }
+                    messageContent.setText("");
+                    Toast.makeText(this, "成功", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this, "手机号码或者短信内容不可以为空", Toast.LENGTH_SHORT).show();
+                }
+            default:
+                break;
+        }
     }
+
     public static ArrayList<MyContacts> getAllContacts(Context context) {
         ArrayList<MyContacts> contacts = new ArrayList<MyContacts>();
 
