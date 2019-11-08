0175373a6d21e8b611ed45d71dce7f58b8c0197c
Dav1dch <807557852@qq.com>
2019-11-08 18:30:48 +0800

    finish

--- app/src/main/java/com/tuffy/dial/ContactAdapter.java
+++ app/src/main/java/com/tuffy/dial/ContactAdapter.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,27 +19,30 @@ import java.util.List;
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
     String[] colorArray = {"#EF9A9A", "#A5D6A7", "#CE93D8", "#9FA8DA", "#90CAF9", "#FFAB91"};
     TextView numText;
+    MainActivity mainActivity;
     private List<MyContacts> myContactsList;
     static class ViewHolder extends RecyclerView.ViewHolder{
        TextView contactName;
        TextView contactPhone;
        TextView Firstletter;
+       ImageButton btn_toMessage;
 
        public ViewHolder(@NonNull View itemView) {
            super(itemView);
            contactName = itemView.findViewById(R.id.contact_name);
            contactPhone = itemView.findViewById(R.id.contact_phoneNum);
            Firstletter = itemView.findViewById(R.id.First_letter);
+           btn_toMessage = itemView.findViewById(R.id.btn_toMessage);
        }
     }
    public  ContactAdapter(List<MyContacts> contactsList){
        myContactsList = contactsList;
    }
 
-   public ContactAdapter(List<MyContacts> contactsList, TextView view){
+   public ContactAdapter(List<MyContacts> contactsList, MainActivity activity){
         myContactsList = contactsList;
-        numText = view;
-
+        numText = activity.phonenum;
+        mainActivity = activity;
    }
 
 
@@ -61,6 +66,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
            @Override
            public void onClick(View v) {
                numText.setText(holder.contactPhone.getText());
+           }
+       });
+       holder.btn_toMessage.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+              mainActivity.message_phone.setText(holder.contactPhone.getText().toString());
+              mainActivity.tabhost.setCurrentTab(1);
            }
        });
 
--- app/src/main/java/com/tuffy/dial/MainActivity.java
+++ app/src/main/java/com/tuffy/dial/MainActivity.java
@@ -19,6 +19,7 @@ import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TabHost;
@@ -26,6 +27,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * @author david
@@ -35,8 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView testText;
     private static final int PERMISS_CONTACT = 1;
     private RecyclerView recyclerView;
-    private TextView phonenum;
-    private TabHost tabhost;
+    protected TextView phonenum;
+    protected TabHost tabhost;
+    protected EditText message_phone;
 
     /*** init function
      *
@@ -51,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initRecyclerView() {
         ArrayList<MyContacts> contacts = getAllContacts(MainActivity.this);
         phonenum = findViewById(R.id.phoneNum);
-        ContactAdapter adapter = new ContactAdapter(contacts, phonenum);
+        ContactAdapter adapter = new ContactAdapter(contacts, this);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setAdapter(adapter);
@@ -59,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initBtn() {
-        findViewById(R.id.dial).setOnClickListener(this);
+        findViewById(R.id.btn_dial).setOnClickListener(this);
         findViewById(R.id.btn_one).setOnClickListener(this);
         findViewById(R.id.btn_two).setOnClickListener(this);
         findViewById(R.id.btn_three).setOnClickListener(this);
@@ -71,6 +76,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_nine).setOnClickListener(this);
         findViewById(R.id.btn_back).setOnClickListener(this);
         findViewById(R.id.btn_sendmessage).setOnClickListener(this);
+        message_phone = findViewById(R.id.message_phone);
     }
 
 
@@ -174,7 +180,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 EditText messagePhone = findViewById(R.id.message_phone);
                 EditText messageContent = findViewById(R.id.message_content);
                 String number = messagePhone.getText().toString();
-                String content = messagePhone.getText().toString();
+                String content = messageContent.getText().toString();
                 if(!"".equals(number) && !"".equals(content)){
                     ArrayList<String> message = SmsManager.getDefault().divideMessage(content);
                     for (String text : message){
@@ -188,6 +194,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+    public  boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = { 0, 0 };
+            //获取输入框当前的location位置
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static ArrayList<MyContacts> getAllContacts(Context context) {
@@ -218,7 +263,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //记得要把cursor给close掉
             phoneCursor.close();
         }
+        Comparator<MyContacts> comparator = new Comparator<MyContacts>() {
+            @Override
+            public int compare(MyContacts o1, MyContacts o2) {
+                char[] char1 = FirstCharUtil.first(o1.getName()).toCharArray();
+                char[] char2 = FirstCharUtil.first(o2.getName()).toCharArray();
+                int result = (int)char1[0] - (int)char2[0];
+                return result;
+            }
+        };
         cursor.close();
+        contacts.sort(comparator);
         return contacts;
     }
+
 }
